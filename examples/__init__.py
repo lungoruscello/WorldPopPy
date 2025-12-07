fda8fc071e2fe341c2adeb8953d9babb86b19ebf
lungoruscello <saschal@ethz.ch>
2025-12-07 20:12:12 +0100

    minor: Add a small example GeoDataFrame for Volcanoes in Southern Kamchatka

--- examples/__init__.py
+++ examples/__init__.py
@@ -5,3 +5,7 @@ from worldpoppy.config import ASSET_DIR
 
 def load_italian_regions():
     return geopandas.read_feather(ASSET_DIR / 'italian_regions_simplified.feather')
+
+
+def load_kamchatka_volcano_example():
+    return geopandas.read_feather(ASSET_DIR / 'kamchatka_petropavlosk_area.feather')
